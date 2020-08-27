4d37230a45406481f85eb83aa0a3d93d35d3a853
Renan.Silva <renan.barbosards@gmail.com>
2020-08-27 12:42:21 -0300

    correção busca locais

--- app/Http/Controllers/SearchInPlaces/SearchInGabinetController.php
+++ app/Http/Controllers/SearchInPlaces/SearchInGabinetController.php
@@ -37,10 +37,13 @@ class SearchInGabinetController extends Controller
             pi.name, pi.order, pi.id as priority_id,                                                /* select tuplas priorities */
             po.registry_id, po.document_type, po.document_number, po.source, po.date, po.subject,   /* select tuplas procedures */
                     
-            count(po.files) as qtd_procedures_files             /* conta qtd de files   */
+            count(po.files) as qtd_procedures_files,             /* conta qtd de files   */
+            count(fm) as qtd_file_managers                          /* conta qtd de files       */
+
             FROM registries r                                   /* da tabela registries */
             join priorities pi on pi.id=r.priority              /* junta com priorities */
             left join procedures po on po.registry_id=r.id      /* junta com procedures */
+            left join file_managers fm on r.id=fm.route_id      /* junta com file_managers */
             left outer join notes n on n.registries_id=r.id     /* junta com notes, busca fora relação */
 
             where r.place=$id /* busca somente fora do cg = 2 */
